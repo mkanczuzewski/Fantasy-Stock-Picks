27c88465edeb076bffaedd593a46d1935ac25b44
mkanczuzewski <mkanczuzewski@gmail.com>
2022-01-23 12:50:29 -0500

    v2 troubleshooting heroku sequalize error

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
+const Sequelize = require('sequelize');
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -11,7 +10,6 @@ const sequelize = process.env.JAWSDB_URL
       dialectOptions: {
         decimalNumbers: true, 
       },
-      port: 3306
     });
 
 module.exports = sequelize;
