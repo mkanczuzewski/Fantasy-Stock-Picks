1b2c663abbec60088538d643aa7f272b632331c2
mkanczuzewski <mkanczuzewski@gmail.com>
2022-01-23 11:46:24 -0500

    corrected date on comments from front page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -12,12 +12,11 @@ router.get('/', (req, res) => {
       'post_text',
       'title',
       'user_id',
-     
     ],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id',],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
           attributes: ['username']
